249de3c46b90eee10bca90d96f0aadec3bbf1ab4
jcurbelo <robin@paella.dev>
2026-02-25 19:03:16 -0500

    Add CheckoutDemo example app
    Minimal SwiftUI app demonstrating CrossmintCheckout integration with
    embedded checkout view, fiat payment config, and appearance rules.

---
+++ Examples/CheckoutDemo/CheckoutDemo/CheckoutDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckoutDemoApp.swift
+//  CheckoutDemo
+//
+//  Created by Robin Curbelo on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct CheckoutDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
